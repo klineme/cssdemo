1c3d7adc1bc349b223d634ec707fc8def605206c
Mattew Kline <mekline.jobs@gmail.com>
2017-06-28 11:30:17 -0400

    added more functionality to js

--- jstest/functions/fn.js
+++ jstest/functions/fn.js
@@ -1,7 +1,11 @@
 var c = console;
 
+window.onload = function(){
+    var el = document.getElementById("main");
+    log(el);
+}
 function log(val){
-    c.log(val)
+    c.log(val);
 }
 
-log("HELLO!!!!!!")
+// log("HELLO!!!!!!");

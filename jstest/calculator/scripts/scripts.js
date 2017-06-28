2a7f0a677527ada29171dab7135556433c73b4a7
Mattew Kline <mekline.jobs@gmail.com>
2017-06-28 17:08:20 -0400

    Added Division and Multiplication after reset func

--- jstest/calculator/scripts/scripts.js
+++ jstest/calculator/scripts/scripts.js
@@ -18,6 +18,7 @@ window.onload = function(){
         //c.log(btn);
         if(btn.length > 1) return;
         else dsp += btn;
+
         if(btn == "="){
             if(left && right){
                 var rint = parseInt(right);
@@ -28,6 +29,12 @@ window.onload = function(){
                 if(op == "-"){
                     result = lint - rint;
                 }
+                if(op == "*"){
+                    result = lint * rint;
+                }
+                if(op == "÷"){
+                    result = lint / rint;
+                }
                 dsp += result.toString();
                 done = true;
             }
@@ -36,7 +43,13 @@ window.onload = function(){
                 return;
             }
         }
-        else if(btn == "+" || btn == "-"){
+        else if(btn == "+" || btn == "-" || btn == "*" || btn == "÷"){
+            if(done){
+                left = result.toString();
+                result = 0;
+                done = false;
+                right = "";
+            }
             if(!left){
                 op = "";
                 dsp = "";
@@ -45,6 +58,14 @@ window.onload = function(){
             op = btn;
         }
         else{
+            if(done){
+                dsp = btn;
+                left = ""; //left side of op
+                op = ""; //operator string buffer
+                right = ""; //right side of op
+                result = 0;
+                done = false;  
+            }
             if(op){
                 right += btn;
             }

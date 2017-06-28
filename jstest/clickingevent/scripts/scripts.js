6e3a5d1b4a7103432352bfd8e7f1f490933d1f78
Mattew Kline <mekline.jobs@gmail.com>
2017-06-28 14:06:56 -0400

    Added doubleclick event to random list

---
+++ jstest/clickingevent/scripts/scripts.js
@@ -0,0 +1,9 @@
+'use strict' // Look this up, enforces good practices like staying var
+var c = console;
+
+window.onload = function(){
+    var list = document.getElementById("Useless");
+    list.addEventListener("dblclick", /* need listener func*/ function(event){
+        c.log(event.target);
+    });
+}

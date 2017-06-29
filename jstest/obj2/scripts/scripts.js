2da753927f96dad56545257c3b14073d5d1d7c11
Mattew Kline <mekline.jobs@gmail.com>
2017-06-29 11:55:42 -0400

    Added 2nd object demo

---
+++ jstest/obj2/scripts/scripts.js
@@ -0,0 +1,9 @@
+"use strict";
+var c = console;
+window.onload = function(){
+    function shape(type, height, width){
+        this.type = type;
+        this.height = height;
+        this.width = width;
+    }
+}

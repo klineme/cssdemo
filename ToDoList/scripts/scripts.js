f96b1bc4374e3175162b23c8daa62d0aa23e14c9
Mattew Kline <mekline.jobs@gmail.com>
2017-06-29 16:42:57 -0400

    Can add to list now

--- ToDoList/scripts/scripts.js
+++ ToDoList/scripts/scripts.js
@@ -1,6 +1,20 @@
 "use strict";
 var c = console;
 window.onload = function(){
-    var todo = [];
+    var inputs = document.getElementById("inputs");
+    var add = document.getElementById("add");
+    var box = document.getElementById("box");
+    var todo = document.getElementById("todo");
     //var done = [];
+    inputs.addEventListener("click", function(event){
+        var clicked = event.target;
+        if(clicked.id == "add"){
+            if(!box.value) return;
+            var newnode = document.createElement("li");
+            c.log("<li>" + box.value + "</li><br>");
+            newnode.innerHTML = ("<li>" + box.value + "</li><input type='button' value='DELETE ME'>");
+            todo.appendChild(newnode);
+            box.value = "";
+        }
+    });
 }

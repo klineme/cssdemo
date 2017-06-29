bea1f50300ca65ab178ea4cbf9ea9f25d477531b
Mattew Kline <mekline.jobs@gmail.com>
2017-06-29 12:13:32 -0400

    Made func that uses shape and added unique area fn

--- jstest/obj2/scripts/scripts.js
+++ jstest/obj2/scripts/scripts.js
@@ -1,9 +1,20 @@
 "use strict";
 var c = console;
 window.onload = function(){
-    function shape(type, height, width){
-        this.type = type;
-        this.height = height;
-        this.width = width;
+    var rect = new Rectangle(2, 38920);
+
+    c.log(rect.Area());
+}
+
+function shape(height, width){
+    this.height = height;
+    this.width = width;
+}
+
+function Rectangle(height, width){
+    shape.call(this, height, width);
+
+    this.Area = function(){
+        return this.height * this.width;
     }
 }

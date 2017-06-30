f3bb6e2bdc74fa8536d085e7f16983b3c35ed203
Mattew Kline <mekline.jobs@gmail.com>
2017-06-30 13:50:21 -0400

    MOre noteS

--- xhr/scripts/scripts.js
+++ xhr/scripts/scripts.js
@@ -2,5 +2,22 @@
 var c = console;
 
 window.onload = function(){
-    
+    var url = "http://pokeapi.co/api/v2/pokemon/1";
+    var url2 = "http://swapi.co/api/people/1";
+    var xhr = new XMLHttpRequest();
+
+    c.log("ReadyState = " + xhr.readyState);
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4 && xhr.status == 200){
+            var data = xhr.responseText;
+            // c.log(JSON.parse(data));
+
+            var main = document.getElementById("main");
+            var obj = JSON.parse(data);
+
+            main.innerHTML = obj.name;
+        }
+    }
+    xhr.open("GET", url, true);
+    xhr.send();
 }

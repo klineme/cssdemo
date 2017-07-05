4fa432d0d5ae7e76a850b30fab0c64858e34dad2
Mattew Kline <mekline.jobs@gmail.com>
2017-07-05 12:26:20 -0400

    Menu now picks one of each dish, ingredients still random

--- PROJECT1/scripts/Menu.js
+++ PROJECT1/scripts/Menu.js
@@ -3,8 +3,8 @@ window.Menu = (function(){
     var ingredients = ["Oyster", "Chicken", "Beef", "Blueberry", "Olive", "Strawberry", "Saurkraut", "Pork", "Bacon", "Sausage", "Kale", "Peanut", "Cauliflower", "Broccoli", "Asparagus", "Lamb", "Rice", "Mango", "Porobello", "Tomato", "Turkey", "Cabbage"];
     var dishesLunch = ["Sandwich", "Quesadilla","Salad", "Macaroni", "Dumplings", "Wrap", "Taco", "Soup"];
     var lunchMenu = [];
-    for(i = 0; i < 10; i++){
-        var newDish = ingredients[Math.floor(Math.random() * ingredients.length)] + " " + dishesLunch[Math.floor(Math.random() * dishesLunch.length)];
+    for(i = 0; i < dishesLunch.length; i++){
+        var newDish = ingredients[Math.floor(Math.random() * ingredients.length)] + " " + dishesLunch[i];
         lunchMenu.push(newDish);
         c.log(newDish);
     }
